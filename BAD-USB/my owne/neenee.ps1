cd9447c8ac7dd44c10ddcaed9651fd66c4e1d954
daninoon <112904263+daninoon@users.noreply.github.com>
2022-11-20 22:29:15 +0100

    Update neenee.ps1

---
+++ BAD-USB/my owne/neenee.ps1
@@ -0,0 +1,3 @@
+$intro = "dani van den hoed , you can eet me bal my friend"
+$OUTRO =  "clas 3a from the hood.... 	and yea curiosity killed the cat....     but satisfaction brought him back.... later $fullName"
+n
