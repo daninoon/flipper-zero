0d4b52dc6aeb71c7520a69649752ed1e242ff6f9
daninoon <112904263+daninoon@users.noreply.github.com>
2022-11-23 23:34:23 +0100

    Update myweb.ps1

--- BAD-USB/my owne/myweb.ps1
+++ BAD-USB/my owne/myweb.ps1
@@ -1,3 +1,6 @@
 $s=New-Object -ComObject SAPI.SpVoice
 $s.Speak("wanna see something really cool?")
-[System.Windows.MessageBox]::Show('Would  you like to play a game?','Game input','YesNoCancel','Error')
+$popmessage = "Hello bal-eter"
+
+
+$readyNotice = New-Object -ComObject Wscript.Shell;$readyNotice.Popup($popmessage)
