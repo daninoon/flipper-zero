92021a8497d1e7fdbb0524c02c3740db4dca7736
daninoon <112904263+daninoon@users.noreply.github.com>
2022-11-23 22:47:27 +0100

    Update neenee.ps1

--- BAD-USB/my owne/neenee.ps1
+++ BAD-USB/my owne/neenee.ps1
@@ -1,2 +1,2 @@
-$intro = "dani van den hoed , you can eet me bal my friend"
-$OUTRO =  "clas 3a from the hood.... 	and yea curiosity killed the cat....     but satisfaction brought him back.... later dani"
+$s=New-Object -ComObject SAPI.SpVoice
+$s.Speak("wanna see something really cool?")
