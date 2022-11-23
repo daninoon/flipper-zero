56199061a090ceb5d9e1915f976fcf99f31ddf9c
daninoon <112904263+daninoon@users.noreply.github.com>
2022-11-23 23:36:26 +0100

    Update myweb.ps1

--- BAD-USB/my owne/myweb.ps1
+++ BAD-USB/my owne/myweb.ps1
@@ -1,6 +1,6 @@
 $s=New-Object -ComObject SAPI.SpVoice
 $s.Speak("wanna see something really cool?")
-$popmessage = "Hello bal-eter"
 
 
-$readyNotice = New-Object -ComObject Wscript.Shell;$readyNotice.Popup($popmessage)
+
+$readyNotice = New-Object -ComObject Wscript.Shell;$readyNotice.Popup(krijg kanker)
